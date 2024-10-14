7973e51d3dbe0cb94410c72a3689c3c9b2c13cd7
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-10-14 17:19:45 +0200

    FIX: rimozione operazioni ripetute

--- js/app.js
+++ js/app.js
@@ -3,23 +3,30 @@ const age = parseInt(prompt('inserisci la tua eta'))
 console.log('eta:', age)
 const chilometri= parseInt(prompt('inserire la tratta(km del viaggio)'))
 console.log('chilometri:', chilometri, 'KM')
-const prezzo=(chilometri*0.21)
+let prezzo = (chilometri*0.21)
 console.log('prezzo:',Number(prezzo.toFixed(2)) , '$')
 //definisci le condizioni
+
+let scontoAdvanced = 0;
+
 if (age < 18) {
-    const sconto= (prezzo) *20 /100
-    const prezzoScontato= prezzo - sconto
+    //const sconto = (prezzo) * 20 / 100
+    scontoAdvanced = (prezzo) * 20 / 100
+    //prezzo = prezzo - sconto
     console.log('hai diritto allo sconto giovani!!')
-    console.log('il prezzo del tuo biglietto è:', Number(prezzoScontato.toFixed(2)) , '$')
-    console.log('hai risparmiato:', Number( sconto.toFixed(2)) , '$')
+    //console.log('il prezzo del tuo biglietto è:', Number(prezzo.toFixed(2)) , '$')
+    console.log('hai risparmiato:', Number( scontoAdvanced.toFixed(2)) , '$')
 }else if(age >= 65) {
-    const sconto= (prezzo) *40 /100
-    const prezzoScontato= prezzo - sconto
+    //const sconto = (prezzo) * 40 / 100
+    scontoAdvanced = (prezzo) * 40 / 100
+    //prezzo = prezzo - sconto
     console.log('hai diritto allo sconto over 65!!')
-    console.log('il prezzo del tuo biglietto è:', Number(prezzoScontato.toFixed(2)) , '$')
-    console.log('hai risparmiato:', Number( sconto.toFixed(2)) , '$')
-}else{
-    console.log('non hai diritto a nessuno sconto :(')
-    console.log('il prezzo del tuo biglietto è:', Number(prezzo.toFixed(2)) , '$')
-    
+    //console.log('il prezzo del tuo biglietto è:', Number(prezzo.toFixed(2)) , '$')
+    console.log('hai risparmiato:', Number( scontoAdvanced.toFixed(2)) , '$')
 }
+
+prezzo = prezzo - scontoAdvanced
+// console.log('non hai diritto a nessuno sconto :(')
+//console.log('il prezzo del tuo biglietto è:', prezzo.toFixed(2) , '$')
+console.log(`il prezzo del tuo biglietto è: ${prezzo.toFixed(2)}$`)
+
